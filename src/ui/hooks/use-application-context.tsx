f8311b5b6f65c5409dfc3e85c594f2463e07ceee
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2025-01-08 14:06:03 +0700

    adjust [useApplicationContext]: check null to throw error

--- src/ui/hooks/use-application-context.tsx
+++ src/ui/hooks/use-application-context.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import { ApplicationContext } from '../context';
+import { getError } from '@/utilities';
 
 export const useApplicationContext = () => {
   const rs = React.useContext(ApplicationContext);
+
+  if (!rs?.context) {
+    throw getError({
+      message: '[useApplicationContext] must be used within a ApplicationContextProvider',
+    });
+  }
+
   return rs.context;
 };
 
