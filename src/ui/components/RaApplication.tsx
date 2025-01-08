99f8c0e9f7e87e1dd5f8ad00b150c2c4df566a4e
Phat Nguyen <tanphat199@gmail.com>
2025-01-08 16:52:58 +0900

    add redux store + suspend fc to main app

--- src/ui/components/RaApplication.tsx
+++ src/ui/components/RaApplication.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { CoreBindings } from '@/common';
 import { Logger } from '@/helpers';
+import { Context } from '@loopback/context';
 import {
   Admin,
   AuthProvider,
@@ -11,13 +12,30 @@ import {
   Resource,
   ResourceProps,
 } from 'react-admin';
+import { Provider as ReduxProvider } from 'react-redux';
 import { Route } from 'react-router-dom';
+import { Store } from 'redux';
 import { ApplicationContext } from '../context';
 import { IApplication } from '../types';
 
+const Wrapper: React.FC<{
+  context: Context;
+  reduxStore: Store;
+  suspense: React.ReactNode;
+  children: React.ReactNode;
+}> = ({ context, reduxStore, suspense, children }) => {
+  return (
+    <ApplicationContext.Provider value={{ context, logger: Logger.getInstance() }}>
+      <ReduxProvider store={reduxStore}>
+        <React.Suspense fallback={suspense}>{children}</React.Suspense>
+      </ReduxProvider>
+    </ApplicationContext.Provider>
+  );
+};
+
 // -----------------------------------------------------------------
 export const RaApplication: React.FC<IApplication> = (props: IApplication) => {
-  const { context, resources, customRoutes, ...raProps } = props;
+  const { context, reduxStore, suspense, resources, customRoutes, ...raProps } = props;
 
   // -------------------------------------------------------------------------------
   const adminProps = React.useMemo(() => {
@@ -36,7 +54,7 @@ export const RaApplication: React.FC<IApplication> = (props: IApplication) => {
 
   // -------------------------------------------------------------------------------
   return (
-    <ApplicationContext.Provider value={{ context, logger: Logger.getInstance() }}>
+    <Wrapper context={context} reduxStore={reduxStore} suspense={suspense}>
       <Admin {...adminProps}>
         {resources?.map((resource: ResourceProps) => {
           return <Resource key={resource.name} {...resource} />;
@@ -48,6 +66,6 @@ export const RaApplication: React.FC<IApplication> = (props: IApplication) => {
           })}
         </CustomRoutes>
       </Admin>
-    </ApplicationContext.Provider>
+    </Wrapper>
   );
 };
--- src/ui/types.ts
+++ src/ui/types.ts
@@ -1,8 +1,12 @@
 import { Context } from '@loopback/context';
 import { AdminProps, ResourceProps } from 'react-admin';
+import { Store } from 'redux';
 
 export interface IApplication extends AdminProps {
   context: Context;
+  reduxStore: Store;
+  suspense: React.ReactNode;
+
   resources: Array<ResourceProps>;
   customRoutes?: Array<{ name: string; path: string; element: React.ReactElement }>;
 }
